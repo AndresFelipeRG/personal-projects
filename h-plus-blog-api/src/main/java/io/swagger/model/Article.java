46d689621208dce10c36668fe788f22e8221b4d2
AndresFelipeRG <andres.rincon@mail.mcgill.ca>
2020-12-29 01:08:36 +0000

    API definition transferred by SwaggerHub

---
+++ h-plus-blog-api/src/main/java/io/swagger/model/Article.java
@@ -0,0 +1,188 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.ArrayList;
+import java.util.List;
+import org.threeten.bp.LocalDate;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Article
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-12-29T01:08:36.558Z[GMT]")
+
+
+public class Article   {
+  @JsonProperty("createdDate")
+  private LocalDate createdDate = null;
+
+  @JsonProperty("tagline")
+  private String tagline = null;
+
+  @JsonProperty("content")
+  private String content = null;
+
+  @JsonProperty("category")
+  private String category = null;
+
+  @JsonProperty("tags")
+  @Valid
+  private List<String> tags = null;
+
+  public Article createdDate(LocalDate createdDate) {
+    this.createdDate = createdDate;
+    return this;
+  }
+
+  /**
+   * Get createdDate
+   * @return createdDate
+   **/
+  @Schema(description = "")
+  
+    @Valid
+    public LocalDate getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(LocalDate createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public Article tagline(String tagline) {
+    this.tagline = tagline;
+    return this;
+  }
+
+  /**
+   * Get tagline
+   * @return tagline
+   **/
+  @Schema(description = "")
+  
+    public String getTagline() {
+    return tagline;
+  }
+
+  public void setTagline(String tagline) {
+    this.tagline = tagline;
+  }
+
+  public Article content(String content) {
+    this.content = content;
+    return this;
+  }
+
+  /**
+   * Get content
+   * @return content
+   **/
+  @Schema(description = "")
+  
+    public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public Article category(String category) {
+    this.category = category;
+    return this;
+  }
+
+  /**
+   * Get category
+   * @return category
+   **/
+  @Schema(description = "")
+  
+    public String getCategory() {
+    return category;
+  }
+
+  public void setCategory(String category) {
+    this.category = category;
+  }
+
+  public Article tags(List<String> tags) {
+    this.tags = tags;
+    return this;
+  }
+
+  public Article addTagsItem(String tagsItem) {
+    if (this.tags == null) {
+      this.tags = new ArrayList<String>();
+    }
+    this.tags.add(tagsItem);
+    return this;
+  }
+
+  /**
+   * Get tags
+   * @return tags
+   **/
+  @Schema(description = "")
+  
+    public List<String> getTags() {
+    return tags;
+  }
+
+  public void setTags(List<String> tags) {
+    this.tags = tags;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Article article = (Article) o;
+    return Objects.equals(this.createdDate, article.createdDate) &&
+        Objects.equals(this.tagline, article.tagline) &&
+        Objects.equals(this.content, article.content) &&
+        Objects.equals(this.category, article.category) &&
+        Objects.equals(this.tags, article.tags);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(createdDate, tagline, content, category, tags);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Article {\n");
+    
+    sb.append("    createdDate: ").append(toIndentedString(createdDate)).append("\n");
+    sb.append("    tagline: ").append(toIndentedString(tagline)).append("\n");
+    sb.append("    content: ").append(toIndentedString(content)).append("\n");
+    sb.append("    category: ").append(toIndentedString(category)).append("\n");
+    sb.append("    tags: ").append(toIndentedString(tags)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
